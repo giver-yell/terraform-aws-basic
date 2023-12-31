d5bd265d7144e84d76848aa56fa0fea3b7961f1b
giver.yell <giver.yell@gmail.com>
2023-12-31 09:53:25 +0900

    ✨ template

--- main.tf
+++ main.tf
@@ -1,18 +1,33 @@
+# --------------------------
+# Terraform configuration
+# --------------------------
+terraform {
+  required_version = ">= 1.6.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31"
+    }
+  }
+}
+
+# --------------------------
+# Provider
+# --------------------------
 provider "aws" {
+  profile = "terraform"
   region = "ap-northeast-1"
 }
 
-resource "aws_instance" "hello-world" {
-  ami = "ami-0dfa284c9d7b2adad"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-
-  user_data = <<EOF
-#!/bin/bash
-amazon-linux-extras install -y nginx1.24
-systemctl start nginx
-EOF
+# --------------------------
+# Variables
+# --------------------------
+variable "project" {
+  type = string
 }
+
+variable "environment" {
+  type = string
+  
+}
+
